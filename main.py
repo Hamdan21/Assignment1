39c9efbfe3485490dfa6f50d9938e57c966f0da7
hamdan <hamdan@hamdans-MacBook-Pro.local>
2021-09-21 13:56:12 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def twoNumsAvg(a,b):
+    return 0.0
+
+def test_twoNumbsAvg():
+    x=twoNumsAvg(4.0,6.0)
+    assert(x==5.0)
+
+def run_all_tests():
+    test_twoNumbsAvg()
+
+def main():
+    run_all_tests()
